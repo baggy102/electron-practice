322f9a5b9970ea020c7c736b373feb8139d28f4a
baggy102 <rlgh0806@gmail.com>
2026-06-09 20:08:38 +0900

    practice/dart-mode, ipcRenderer, ipcMainHandle 동작 확인

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
-
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
 const path = require('node:path')
 
-const createWindow = () => {
+function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -10,10 +9,35 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+
   win.loadFile('index.html')
-  win.webContents.openDevTools()
 }
+
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
+
 app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
